bb01bc04d3f95bcb35484ae105a296dff56894ed
oogatta <oogatta@gmail.com>
2013-08-24 22:21:38 +0900

    ざっくり動く

--- tasks/dotfile_linker.js
+++ tasks/dotfile_linker.js
@@ -9,12 +9,33 @@
 'use strict';
 
 module.exports = function(grunt) {
-    grunt.registerMultiTask('dotfile_linker', 'create symbolic links for dotfiles.', function() {
-        // Merge task-specific and/or target-specific options with these defaults.
-        var options = this.options({});
+    var fs   = require('fs');
+    var path = require('path');
 
-        // Iterate over all specified file groups.
-        this.files.forEach(function(f) {
+    grunt.registerMultiTask('dotfile_linker', 'create symbolic links for dotfiles.', function() {
+        this.files.forEach(function(file) {
+            if ( !fs.existsSync(file.dest) || !fs.statSync(file.dest).isDirectory() ) {
+                grunt.log.write('destination directory does not exits or is not a directory.');
+                return;
+            }
+
+            file.src.map(function(src){
+                return {
+                    fullPath : src,
+                    linkname : '.' + path.basename(src, '.dot')
+                };
+            }).forEach(function(target){
+                var link     = path.join(file.dest, target.linkname);
+                var existing = path.resolve(target.fullPath);
+
+                if ( fs.existsSync(link) && fs.lstatSync(link).isSymbolicLink() ) {
+                    grunt.log.writeln('skip:' + link);
+                }
+                else {
+                    fs.symlinkSync(existing, link);
+                    grunt.log.writeln('created:' + link);
+                }
+            });
         });
     });
 };
